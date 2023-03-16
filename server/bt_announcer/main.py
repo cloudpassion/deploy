bac46ad180cf37e615ea8b6b4f82d58d72bc9817
clp <git@cldvp.xyz>
2023-03-16 10:54:50 +0000

    add check for main process for sanic not create second instance

--- server/bt_announcer/main.py
+++ server/bt_announcer/main.py
@@ -1,22 +1,27 @@
 import asyncio
+#import logging
 
 from config import settings
 
 from web import WEB
 
+#logging.getLogger("sanic.access").propagate = False
+#logging.getLogger("sanic.root").propagate = False
 
 async def main():
     # server
     server = WEB()
 
     await server.initialize()
+    
+    if __name__ == "__main__":
+        server.app.run(
+            host=settings.http.host, port=settings.http.port,
+        )
 
-    server.app.run(
-        host=settings.http.host, port=settings.http.port,
-    )
-
-    while True:
-        await asyncio.sleep(0)
+        while True:
+            await asyncio.sleep(0)
 
 
 asyncio.run(main())
+
