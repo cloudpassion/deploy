dfb5f4c84134c73a11dc6a2cb1b592eb2c36838e
clp <git@cldvp.xyz>
2023-04-24 21:37:32 +0000

    docker changes

--- monitor/tr_updater/tr_update.py
+++ monitor/tr_updater/tr_update.py
@@ -4,18 +4,13 @@ from log import logger
 
 from app import TruckersMPUpdater
 
-# TIMEOUT = 60*10
-# TIMEOUT = 0
-
 
 async def main():
 
-    # while TIMEOUT:
     logger.info(f'tr_start')
     updater = TruckersMPUpdater()
 
     await updater.update()
-    # await asyncio.sleep(TIMEOUT)
 
 
 asyncio.run(main())
