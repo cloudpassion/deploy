32a1e2a6d0c1f385679ab2d2e64b9fe4fe532dfb
clp <git@cldvp.xyz>
2023-05-21 14:08:30 +0000

    merge code, clear secrets

---
+++ tg/tg_manage_old/run_django.py
@@ -0,0 +1,4 @@
+from rerogram.django_telegram.manage import main
+
+main()
+
---
+++ tg/tg_manage_old/run_manage_old.py
@@ -0,0 +1,111 @@
+from atiny.reos.cd import cd
+cd(__file__)
+
+import asyncio
+
+from log import logger, log_stack
+
+try:
+    import uvloop
+    uvloop.install()
+except ImportError:
+    logger.info(f'uvloop not found')
+
+
+from datetime import datetime, timedelta
+from collections import deque
+
+from rerogram.loader import setup_django
+from rerogram.client import MyTelegramClient
+
+from log import logger
+from config import settings
+
+
+async def chunked_check_history(
+        tg_client, chat_id, limit=98, to_watch=30000,
+        sleep=0.0,
+):
+
+    while to_watch >= 0:
+
+        await tg_client.on_start(
+            None,
+            tg_client.db_insert_messages(
+                chat_id,
+                tg_client.discussion_fwd_data,
+                limit=limit,
+                sleep=sleep,
+                offset=to_watch,
+            )
+        )
+
+        to_watch -= limit
+
+
+async def main():
+
+    tg_client = MyTelegramClient()
+
+    # chunked if more than ~500 new message
+    for _fwd_data in (
+            # (98, 500, settings.discussion.forward.sh1_chat.from_chat_id),
+            # (98, 3000, settings.discussion.forward.sh2_chat.from_chat_id),
+            # (98, 700, settings.discussion.forward.oss_chat.from_chat_id),
+            None,
+    ):
+        if not _fwd_data:
+            continue
+
+        limit = _fwd_data[0]
+        to_watch = _fwd_data[1]
+        w_chat_id = _fwd_data[2]
+        await chunked_check_history(
+            tg_client,
+            chat_id=w_chat_id,
+            limit=limit,
+            to_watch=to_watch,
+            sleep=0.2,
+        )
+
+    # rewatch for get newest before monitor
+    # chat_id from settings file settings.discussion.forward
+    for _fwd_data in (
+        # name, limit, chat_id (*optional*)
+        ('sh2_chat', 128),
+        ('sh1_chat', 128),
+        ('oss_chat', 128),
+
+        ('shduet_channel', 64),
+        ('ukprok_channel', 64),
+
+        ('spb_channel', 32, settings.spb.tg.channel_id),
+        ('spb_chat', 64),
+    ):
+        re_chat_name = _fwd_data[0]
+        to_watch = _fwd_data[1]
+
+        try:
+            re_chat_id = _fwd_data[2]
+        except IndexError:
+            re_chat = getattr(settings.discussion.forward, re_chat_name)
+            re_chat_id = getattr(re_chat, 'from_chat_id')
+
+        await tg_client.on_start(
+            None,
+            tg_client.db_insert_messages(
+                re_chat_id,
+                tg_client.discussion_fwd_data,
+                offset_id='last',
+                limit=to_watch,
+            )
+        )
+
+    await tg_client.on_start(
+        None,
+        tg_client.events()
+        )
+    await tg_client.client_start()
+
+
+asyncio.run(main())
