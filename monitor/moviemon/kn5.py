5170c3f31334d7e19d23b7968b8317f0a84f45ba
clp <git@cldvp.xyz>
2023-06-04 13:17:41 +0000

    some rewrites of get movies list and watch this lists

--- monitor/moviemon/kn5.py
+++ monitor/moviemon/kn5.py
@@ -1,24 +1,80 @@
-#!/usr/bin/python3
-
-import os
 import time
+import random
+import asyncio
 
-from my.sql import *
-from my.tg.tg import *
+from datetime import datetime
+
+from sql import sql_init
+from otg import tg_init, tg_update
+
+from kn.monmov.kinozal import KinozalMonitor
+from kn.kinozaltv.site import KinozalSite
+
 
 sql_init()
 tg_init()
+# tg_update()
 
-from my.getfilms import *
-from my.kinozal import *
 
-tg_update()
+async def test():
 
-TIMEOUT=3600*1
+    return
+    # t = KinozalSite()
+    #
+    # await t.get_details(1978519)
+    # quit()
 
-while True:
-    print('tstart')
-    main_kinozal_releases()
-    print('mex')
-    time.sleep(TIMEOUT)
+    kn = KinozalMonitor()
 
+    await kn.main_releases(year=2023)
+    # await kn.deep_releases(title_year=2022, start='It Came from Somewhere')
+    # await kn.deep_releases(title_year=2023)
+    #
+    quit()
+
+
+async def main():
+
+    deep_check = False
+
+    while True:
+
+        hours = 1
+        timeout = 3600 * hours + random.randint(500, 1000)
+
+        kn = KinozalMonitor()
+
+        tm = datetime.now()
+        cur_year = tm.year
+        pre_year = cur_year - 1
+
+        day = tm.day
+
+        if deep_check:
+
+            deep_check = False
+
+            print(f'deep start')
+            for year in (
+                    pre_year,
+                    cur_year,
+            ):
+                continue
+                # await kn.deep_releases(title_year=year)
+
+            print(f'deep end')
+
+        print(f'main start')
+        await kn.main_releases(year=cur_year)
+        print(f'main end')
+
+        time.sleep(timeout)
+
+        if datetime.now().day != day:
+            deep_check = True
+
+
+asyncio.run(test())
+asyncio.run(main())
+
+         
