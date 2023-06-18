a9dcd80e3e4d9907946fe3f1d3c9cdb1c00a839d
clp <git@cldvp.xyz>
2023-06-18 08:40:57 +0000

    add kinorium upcoming

--- monitor/moviemon/get.py
+++ monitor/moviemon/get.py
@@ -10,6 +10,22 @@ tg_init()
 from kn.getmov import GetMovies
 
 
+async def test():
+
+    for lang in ('ru', 'en', ):
+        kn = GetMovies(
+            kinorium_lang=lang
+        )
+
+        await kn.kinorium_get_movies_upcoming(
+            year=2023,
+            max_page=1,
+            skip_write=True
+        )
+
+    quit()
+
+
 async def main():
 
     kn = GetMovies()
@@ -25,5 +41,16 @@ async def main():
         await kn.kinorium_filmlist(year=year)
         await kn.imdb_year_list(year=year)
 
+    for lang in ('ru', 'en', ):
+        kn = GetMovies(
+            kinorium_lang=lang
+        )
 
+        await kn.kinorium_get_movie_premiers(
+            year=cur_year,
+            max_page=1,
+            # skip_write=True
+        )
+
+asyncio.run(test())
 asyncio.run(main())
